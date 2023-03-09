ccdf1c326e697ec40ef2bad2cefbcecd7060b496
gl_sokolov <gl_sokolov@mail.ru>
2023-03-09 19:53:09 +0300

    Домашка выполнена

--- src/main/java/com/example/hwtesting/model/Users.java
+++ src/main/java/com/example/hwtesting/model/Users.java
@@ -1,7 +1,40 @@
 package com.example.hwtesting.model;
 
+import jakarta.xml.bind.annotation.XmlType;
 import lombok.*;
-
+@Getter
+@EqualsAndHashCode
 public class Users {
 
+    public static final String DEFAULT_LOGIN = "user";
+    public static final String DEFAULT_EMAIL = "qwerty@mail.ru";
+
+    String login;
+    String email;
+
+    public Users(String login, String email) {
+        setLogin(login);
+        setEmail(email);
+    }
+
+    public void setLogin(String login) {
+        if (!login.isBlank() && !login.isEmpty()) {
+            this.login = login;}
+        else {
+            this.login = DEFAULT_LOGIN;
+        }
+    }
+
+    public void setEmail(String email) {
+        if (!email.isBlank() && email.length() > 8) {
+            this.email = email;}
+        else {
+            this.email = DEFAULT_EMAIL;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "login: "+login+", email: "+email;
+    }
 }
--- src/test/java/com/example/hwtesting/service/impl/UsersServiceImplTest.java
+++ src/test/java/com/example/hwtesting/service/impl/UsersServiceImplTest.java
@@ -8,5 +8,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsersServiceImplTest {
+    public static final String EMPTY_USER = "";
+    public static final String SPACE_USER = "   ";
+    public static final String CORRECT_EMAIL = "qwerty@mail.ru";
+    public static final String INCORRECT_EMAIL_1 = "qwerty@mailru";
+    public static final String INCORRECT_EMAIL_2 = "qwertymail.ru";
+    public static final String CORRECT_LOGIN = "user";
+    public static final String INCORRECT_LOGIN = "qwerty@mail.ru";
+    public static final Users CORRECT_USER = new Users("user", "qwerty@mail.ru");
 
+    static String login;
+    static String email;
+    UsersServiceImpl usersServiceOut = new UsersServiceImpl();
+
+    @Test
+    void shouldCreateUserWithCorrectParameters() {
+        Users result = usersServiceOut.createUser(CORRECT_LOGIN, CORRECT_EMAIL);
+        assertEquals(result, CORRECT_USER);
+    }
+
+    @Test
+    void shouldCreateUserWhenParametersIsNotExist() {
+        Users result = usersServiceOut.createUser();
+        assertEquals(result, CORRECT_USER);
+    }
+
+    @Test
+    void shouldReadTheCorrectParameter() {
+        Users result = usersServiceOut.createUser(CORRECT_LOGIN, CORRECT_EMAIL);
+//        Users result = usersServiceOut.createUser(CORRECT_LOGIN, INCORRECT_EMAIL_1);
+//        Users result = usersServiceOut.createUser(CORRECT_LOGIN, INCORRECT_EMAIL_2);
+        Assertions.assertTrue(result.getEmail().contains("@"));
+        Assertions.assertTrue(result.getEmail().contains("."));
+    }
+    @Test
+    void shouldNotBeEqual() {
+        Users result = usersServiceOut.createUser(CORRECT_LOGIN, CORRECT_EMAIL);
+//        Users result = usersServiceOut.createUser(INCORRECT_LOGIN, CORRECT_EMAIL);
+        assertNotEquals(result.getLogin(), result.getEmail());
+    }
 }
